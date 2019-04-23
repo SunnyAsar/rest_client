c0c7bd993e3a9cad54a5e4850c563e0ea1607e41
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2019-04-23 11:43:36 -0500

    [add] readme and format to main.rb

---
+++ README.md
@@ -0,0 +1,8 @@
+# Rest Client practice
+
+some practice with rest-client gem
+
+## Contributors
+
+- [Henry Tabima](https://github.com/HenryTabima)
+- [Sunny Asar](https://github.com/SunnyAsar)
--- bin/main.rb
+++ bin/main.rb
@@ -1,10 +1,12 @@
 require 'rest-client'
 
-Base_uri = 'https://www.bing.com/search?q='
+base_uri = 'https://www.bing.com/search?q='.freeze
 
 puts 'what do you want to find'
-answer = gets.chomp()    
-result = RestClient.get(Base_uri+answer)
+answer = gets.chomp
+result = RestClient.get(base_uri + answer)
 
 p result.code
 p result
+
+# Henry wus here
