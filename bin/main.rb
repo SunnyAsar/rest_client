4d34204807d7be32828c66c4ed70dd3bf65b5203
Sunny <ring2rings93@gmail.com>
2019-04-23 17:24:48 +0100

    get request with rest-client

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,10 @@
 require 'rest-client'
+
+Base_uri = 'https://www.bing.com/search?q='
+
+puts 'what do you want to find'
+answer = gets.chomp()    
+result = RestClient.get(Base_uri+answer)
+
+p result.code
+p result
